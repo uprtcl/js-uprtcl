a4f4548520293baab698df248e34edd6303e470d
Guillem Cordoba <guillem.cordoba@gmail.com>
2019-07-23 14:22:15 +0200

    Test WIP

--- test/services/cached-provider.service.test.ts
+++ test/services/cached-provider.service.test.ts
@@ -1,7 +1,10 @@
 import { CacheSourceMock } from '../mocks/cache-source.mock';
 import { CachedProviderService } from '../../src/services/cached-remotes/cached-provider.service';
 import { TaskQueueMock } from '../mocks/task.queue.mock';
-import { TaskQueue } from '../../src/utils/task.queue';
+
+const object1 = {
+  links: ['object2']
+};
 
 /**
  * CachedProviderService test
@@ -10,15 +13,40 @@ describe('CachedProviderService test', () => {
   let cachedProvider: CachedProviderService<CacheSourceMock, CacheSourceMock, CacheSourceMock>;
   let cache: CacheSourceMock;
   let remote: CacheSourceMock;
+  let spyCacheGet: jest.SpyInstance;
+  let spyCacheCreate: jest.SpyInstance;
+  let spyRemoteGet: jest.SpyInstance;
+  let spyQueueTask: jest.SpyInstance;
 
   beforeEach(() => {
     cache = new CacheSourceMock();
     remote = new CacheSourceMock();
 
+    spyCacheGet = jest.spyOn(cache, 'get');
+    spyCacheCreate = jest.spyOn(cache, 'create');
+    spyRemoteGet = jest.spyOn(remote, 'get');
+
+    const taskQueue = new TaskQueueMock();
+    spyQueueTask = jest.spyOn(taskQueue, 'queueTask');
+
     cachedProvider = new CachedProviderService<CacheSourceMock, CacheSourceMock, CacheSourceMock>(
       cache,
       remote,
-      new TaskQueueMock() as TaskQueue
+      taskQueue as any
     );
+  });
+
+  it('optimisticCreate executes in the cache and schedules task', async () => {
+    expect(
+      cachedProvider.optimisticCreate(
+        object1,
+        service => service.create('object1', object1),
+        service => service.create('object1', object1)
+      )
+    ).toBe('object1');
+
+    expect(spyCacheCreate).toHaveBeenCalledTimes(1);
+    expect(spyRemoteGet).toHaveBeenCalledTimes(0);
+    expect(spyQueueTask).toHaveBeenCalledTimes(1);
   });
 });
