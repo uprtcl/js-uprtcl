1b28e1a0946453eebdaab9e7c1abf657f19bc534
cbrzn <cesarbrazon10@gmail.com>
2019-11-22 09:48:37 +0100

    Adding style to wiki

--- modules/wikis/src/lenses/wiki-node.lens.ts
+++ modules/wikis/src/lenses/wiki-node.lens.ts
@@ -1,4 +1,4 @@
-import { LitElement, property, html } from 'lit-element';
+import { LitElement, property, html, css } from 'lit-element';
 import { moduleConnect } from '@uprtcl/micro-orchestrator';
 import { LensElement } from '@uprtcl/lenses';
 import { EveesTypes } from '@uprtcl/evees';
@@ -14,20 +14,20 @@ export class WikiNodeLens extends moduleConnect(LitElement) implements LensEleme
   rootHash!: String
 
   createPage = async () => {
-    const perspectivePattern:any = this.request(EveesTypes.PerspectivePattern);
-    const pagePattern:any = this.request(DocumentsTypes.TextNodePattern);
+    const perspectivePattern: any = this.request(EveesTypes.PerspectivePattern);
+    const pagePattern: any = this.request(DocumentsTypes.TextNodePattern);
 
-    const eveesProvider : any = this.requestAll(EveesTypes.EveesRemote)
-    .find((provider:any) => {
-      const regexp = new RegExp('^http');
-      return regexp.test(provider.service.uprtclProviderLocator);
-    });
+    const eveesProvider: any = this.requestAll(EveesTypes.EveesRemote)
+      .find((provider: any) => {
+        const regexp = new RegExp('^http');
+        return regexp.test(provider.service.uprtclProviderLocator);
+      });
 
-    const pagesProvider : any = this.requestAll(DocumentsTypes.DocumentsRemote)
-    .find((provider:any) => {
-      const regexp = new RegExp('^http');
-      return regexp.test(provider.service.uprtclProviderLocator);
-    });
+    const pagesProvider: any = this.requestAll(DocumentsTypes.DocumentsRemote)
+      .find((provider: any) => {
+        const regexp = new RegExp('^http');
+        return regexp.test(provider.service.uprtclProviderLocator);
+      });
 
     const pageHash = await pagePattern.create(
       {},
@@ -59,28 +59,69 @@ export class WikiNodeLens extends moduleConnect(LitElement) implements LensEleme
 
   render() {
     return html`
-      <h4>${this.data.title}</h4>
-      <ul>
-        ${this.data.pages.map(page => {
+    <div class="row">
+      <div class="column left" style="background-color:#aaa;">
+        <h2>${this.data.title}</h2>
+        <ul>
+          ${this.data.pages.map(page => {
           return html`
-            <li @click=${() => this.setPage(page)}>${page}</li>
+          <li @click=${() => this.setPage(page)}>${page}</li>
           `;
-        })}
-      </ul>
+          })}
+        </ul>
 
-      <mwc-button @click=${() => this.createPage()}>
-        <mwc-icon>note_add</mwc-icon>
+    <mwc-button @click=${() => this.createPage()}>
+      <mwc-icon>note_add</mwc-icon>
         new page
-      </mwc-button>
+    </mwc-button>
+
+    </div>
+      <div class="column right" style="background-color:#bbb;">
+        <p>      ${this.rootHash
+          ? html`
+              <cortex-entity .hash=${this.rootHash}></cortex-entity>
+            `
+          : html`
+              Loading...
+            `}</p>
+      </div>
+  </div>
+
+    `;
+  }
 
 
-      ${this.rootHash
-        ? html`
-            <cortex-entity .hash=${this.rootHash}></cortex-entity>
-          `
-        : html`
-            Loading...
-          `}
+  static get styles() {
+    return css`
+    * {
+      box-sizing: border-box;
+    }
+
+    li {
+      word-wrap: break-word;
+      margin-bottom: 9px;
+      cursor: pointer;
+    }
+    
+    .column {
+      float: left;
+      padding: 10px;
+      height: 100vh; /* Should be removed. Only for demonstration */
+    }
+    
+    .left {
+      width: 25%;
+    }
+    
+    .right {
+      width: 75%;
+    }
+
+    .row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
     `;
   }
 }
