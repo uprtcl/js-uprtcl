b00ab9cda7d027c456c8662e8b472937a50360c7
guillem <guillem.cordoba@gmail.com>
2020-04-09 19:49:29 +0200

    Fix test

--- modules/documents/karma.conf.ts
+++ modules/documents/karma.conf.ts
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = config =>
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     // ## code coverage config
     coverageIstanbulReporter: {
       reports: ['lcovonly', 'text-summary'],
@@ -54,7 +54,7 @@ module.exports = config =>
         ]
       }
     },
-    singleRun: false,
+    singleRun: true,
     concurrency: Infinity,
 
     plugins: [
