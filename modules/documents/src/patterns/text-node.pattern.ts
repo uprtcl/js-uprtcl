1f70ddfafaac385bd9e7ac3dba20b0cfb6ede905
guillem <guillem.cordoba@gmail.com>
2020-04-10 10:18:28 +0200

    Fixed types

--- modules/documents/src/patterns/text-node.pattern.ts
+++ modules/documents/src/patterns/text-node.pattern.ts
@@ -24,10 +24,38 @@ import { Lens, HasLenses } from '@uprtcl/lenses';
 import { ApolloClientModule } from '@uprtcl/graphql';
 import { CidConfig } from '@uprtcl/multiplatform';
 
-import { TextNode, TextType } from '../types';
+import { TextNode, TextType, DocNode, DocNodeEventsHandlers } from '../types';
 import { DocumentsBindings } from '../bindings';
+import { DocNodeLens } from './document-patterns';
 
 const propertyOrder = ['text', 'type', 'links'];
+
+const textToTextNode = (textNode: TextNode, text: string): TextNode => {
+  return {
+    ...textNode,
+    text: text
+  };
+};
+
+const typeToTextNode = (textNode: TextNode, type: TextType): TextNode => {
+  return {
+    ...textNode,
+    type: type
+  };
+};
+
+const nodeLevel = (node: DocNode) => {
+  let level = 1;
+  let parent: DocNode | undefined = node;
+  parent = parent.parent;
+
+  while (parent !== undefined) {
+    level = level + 1;
+    parent = parent.parent;
+  }
+
+  return level;
+};
 
 export class TextNodePattern extends Pattern<Entity<TextNode>> {
   recognize(object: object): boolean {
@@ -64,13 +92,57 @@ export class TextNodeCommon
         type: 'content',
         render: (entity: Entity<any>, context: any) => {
           return html`
-            <documents-text-node .data=${node} ref=${entity.id}> </documents-text-node>
+            <documents-text-node
+              .data=${node}
+              ref=${entity.id}
+              color=${context.color}
+              index=${context.index}
+              .genealogy=${context.genealogy}
+              toggle-action=${context.toggleAction}
+              .action=${context.action}
+            >
+            </documents-text-node>
           `;
         }
       }
     ];
   };
 
+  /** lenses top is a lense that dont render the node children, leaving the job to an upper node tree controller */
+  docNodeLenses = (): DocNodeLens[] => {
+    return [
+      {
+        name: 'documents:document',
+        type: 'content',
+        render: (node: DocNode, events: DocNodeEventsHandlers) => {
+          // logger.log('lenses: documents:document - render()', { node });
+          return html`
+            <documents-text-node-editor
+              type=${node.draft.type}
+              init=${node.draft.text}
+              to-append=${node.append}
+              level=${nodeLevel(node)}
+              editable=${node.editable ? 'true' : 'false'}
+              focus-init=${node.focused}
+              @focus=${events.focus}
+              @blur=${events.blur}
+              @content-changed=${(e) => events.contentChanged(textToTextNode(node.draft, e.detail.content), false)}
+              @enter-pressed=${(e) => events.split(e.detail.content, e.detail.asChild)}
+              @backspace-on-start=${(e) => events.joinBackward(e.detail.content)}
+              @delete-on-end=${(e) => events.pullDownward()}
+              @keyup-on-start=${events.focusBackward}
+              @keydown-on-end=${events.focusDownward}
+              @lift-heading=${events.lift}
+              @change-type=${(e) => events.contentChanged(typeToTextNode(node.draft, e.detail.type), e.detail.lift)}
+              @content-appended=${events.appended}
+            >
+            </documents-text-node-editor>
+          `;
+        }
+      }
+    ];
+  };
+  
   merge = (originalNode: Entity<TextNode>) => async (
     modifications: Entity<TextNode>[],
     mergeStrategy: MergeStrategy,
