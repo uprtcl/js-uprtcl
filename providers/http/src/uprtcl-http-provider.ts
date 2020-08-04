1e680d7adeff648f85981493ca09307701971904
Andres <andresg5997@gmail.com>
2020-08-04 18:41:41 +0200

    added auth0 to http provider

--- providers/http/src/uprtcl-http-provider.ts
+++ providers/http/src/uprtcl-http-provider.ts
@@ -5,4 +5,5 @@ export { HttpConnection } from './http.connection';
 export { HttpProvider } from './http.provider';
 export { HttpStore } from './http.store';
 export { HttpEthAuthProvider } from './http-eth-auth.provider';
+export { HttpAuth0Provider } from './http-auth0.provider';
 export { KnownSourcesHttp } from './known-sources.http';
