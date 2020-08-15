50580d6afe696e45946a9511db67f12deaf5410f
Pepo <pepo.ospina@gmail.com>
2020-08-15 09:46:03 +0200

    wip

--- modules/common-ui/src/common-ui.module.ts
+++ modules/common-ui/src/common-ui.module.ts
@@ -1,16 +1,18 @@
 import { MicroModule } from '@uprtcl/micro-orchestrator';
 
+import { CommonUIBindings } from './bindings';
+
+import { Button } from '@material/mwc-button';
+
 export class CommonUIModule extends MicroModule {
   static id = 'common-ui-module';
+  static bindings = CommonUIBindings;
 
   async onLoad() {
-    // customElements.define('uprtcl-mwc-button', Button);
+    customElements.define('uprtcl-mwc-button', Button);
   }
 
   get submodules() {
-    return [
-    ];
+    return [];
   }
-
-  static bindings = undefined;
 }
