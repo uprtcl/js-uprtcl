915c3b2a3000ce0dc20d98a8e35445f3859e658d
Pepo <pepo.ospina@gmail.com>
2020-08-16 10:51:45 +0200

    button icon basic

--- modules/common-ui/src/elements/button.ts
+++ modules/common-ui/src/elements/button.ts
@@ -1,19 +1,37 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css, property } from 'lit-element';
 import { cssFramework } from './miligram.css';
+import { icons } from './icons';
 
 export class UprtclButton extends LitElement {
+  @property({ type: String })
+  icon!: string;
+
   render() {
-    return html`<div class="button"><slot></slot></div>`;
+    return html` <div class="button">
+      ${this.icon !== undefined
+        ? html`<div class="icon-container">${icons[this.icon]}</div>`
+        : ''}<slot></slot>
+    </div>`;
   }
 
   static get styles() {
     return [
       cssFramework,
       css`
-        :host {
-          display: block;
+        svg {
+          fill: white;
+        }
+        .icon-container {
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          margin-right: 10px;
         }
         .button {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
           width: calc(100% - 6rem - 0.2rem);
         }
       `,
