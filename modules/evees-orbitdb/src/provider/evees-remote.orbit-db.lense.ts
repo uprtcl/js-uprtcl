6ae4cb0850114259b973ed1cf7601b0db058beb4
Pepo <pepo@uprtcl.io>
2020-10-07 11:42:38 +0200

    first draft

---
+++ modules/evees-orbitdb/src/provider/evees-remote.orbit-db.lense.ts
@@ -0,0 +1,48 @@
+import { LitElement, property, html, css } from 'lit-element';
+import { ApolloClient } from 'apollo-boost';
+
+import { moduleConnect } from '@uprtcl/micro-orchestrator';
+import { ApolloClientModule } from '@uprtcl/graphql';
+import { EveesModule, EveesHelpers, EveesRemote } from '@uprtcl/evees';
+
+import { EveesOrbitDB } from './evees.orbit-db';
+
+export class RemoteOrbitdDbLense extends moduleConnect(LitElement) {
+  @property({ attribute: false })
+  loading: boolean = false;
+
+  client!: ApolloClient<any>;
+  remote!: EveesOrbitDB;
+
+  async firstUpdated() {
+    this.client = this.request(ApolloClientModule.bindings.Client);
+    this.load();
+  }
+
+  async load() {
+    this.loading = true;
+    this.remote = (this.requestAll(EveesModule.bindings.EveesRemote) as EveesRemote[]).find(r =>
+      r.id.includes('orbitdb')
+    ) as EveesOrbitDB;
+    await this.remote.ready();
+
+    this.loading = false;
+  }
+
+  render() {
+    if (this.loading) {
+      return html`
+        <uprtcl-loading></uprtcl-loading>
+      `;
+    }
+    return html`
+      <div class="">
+        ODB
+      </div>
+    `;
+  }
+
+  static get styles() {
+    return css``;
+  }
+}
--- modules/evees/src/services/evees.remote.ts
+++ modules/evees/src/services/evees.remote.ts
@@ -2,10 +2,12 @@ import { EveesProvider } from './evees.provider';
 import { ProposalsProvider } from './proposals.provider';
 import { Remote } from '../remote';
 import { AccessControlService } from './evees.access-control';
+import { Lens } from '@uprtcl/lenses';
 
 export interface EveesRemote extends EveesProvider, Remote {
   accessControl: AccessControlService;
   proposals?: ProposalsProvider;
 
   canWrite(uref: string): Promise<boolean>;
+  lense?(): Lens;
 }
