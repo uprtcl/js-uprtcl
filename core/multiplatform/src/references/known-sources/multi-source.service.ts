95631e59d16c101ee24458e04f2ff5afc4671e36
Pepo <pepo@uprtcl.io>
2020-12-31 11:35:16 +0100

    wip adapting multi-source

--- core/multiplatform/src/references/known-sources/multi-source.service.ts
+++ core/multiplatform/src/references/known-sources/multi-source.service.ts
@@ -1,97 +1,51 @@
-import { multiInject, inject, injectable, optional } from 'inversify';
-import {
-  PatternRecognizer,
-  CortexModule,
-  Pattern,
-  HasLinks,
-  Entity,
-} from '@uprtcl/cortex';
-import { Dictionary, Logger } from '@uprtcl/micro-orchestrator';
+import { Pattern, HasLinks, Entity } from '@uprtcl/cortex';
+import { Logger } from '@uprtcl/micro-orchestrator';
 
 import { CASSource } from '../../types/cas-source';
 import { KnownSourcesService } from './known-sources.service';
 import { raceToSuccess, discoverKnownSources } from './discovery.utils';
-import { DiscoveryBindings, CASBindings } from '../../bindings';
 import { KnownSourcesSource } from './known-sources.source';
 
 @injectable()
 export class MultiSourceService {
   protected logger = new Logger('MultiSourceService');
-
-  services: Dictionary<CASSource>;
+  protected sourcesMap = new Map<string, CASSource>();
 
   /**
-   * @param recognizer the pattern recognizer to interact with the objects and their links
    * @param localKnownSources local service to store all known sources to be able to retrieve the object afterwards
-   * @param serviceProviders array of all source service providers from which to get objects
+   * @param sources array of all stores from which to get objects
    */
-  constructor(
-    @inject(CortexModule.bindings.Recognizer)
-    protected recognizer: PatternRecognizer,
-    @inject(DiscoveryBindings.LocalKnownSources)
-    public localKnownSources: KnownSourcesService,
-    @inject(EveesClientModule.bindings.Client)
-    public client: EveesClient,
-    @multiInject(CASBindings.CASSource)
-    sources: Array<CASSource>,
-    @multiInject(DiscoveryBindings.DefaultSource)
-    @optional()
-    protected defaultSources: Array<string> | undefined
-  ) {
+  constructor(public localKnownSources: KnownSourcesService, sources: Array<CASSource>) {
     // Build the sources dictionary from the resulting names
-    this.services = sources.reduce(
-      (services, service) => ({ ...services, [service.casID]: service }),
-      {}
-    );
+    sources.forEach((source) => this.sourcesMap.set(source.casID, source));
   }
 
-  /**
-   * @override
-   */
   public async ready(): Promise<void> {
-    const promises = Object.keys(this.services).map((serviceName) =>
-      this.services[serviceName].ready()
-    );
-    await Promise.all(promises);
+    await Promise.all(Array.from(this.sourcesMap.values()).map((source) => source.ready()));
   }
 
   /**
    * @returns gets the casID of all the sources
    */
   public getAllCASIds(): string[] {
-    return Object.keys(this.services);
+    return Array.from(this.sourcesMap.keys());
   }
 
   /**
-   * @returns gets all the services
+   * @returns gets all the sources
    */
   public getAllCASSources(): CASSource[] {
-    return Object.keys(this.services).map((casID) => this.services[casID]);
+    return Array.from(this.sourcesMap.values());
   }
 
   /**
    * Gets the service with the given name
    *
-   * @param source the source name that identifies the service provider
+   * @param casID the source name that identifies the service provider
    * @returns the source identified with the given name
    */
-  public getSource(casID: string | undefined): CASSource {
-    const sources: string[] = this.getAllCASIds();
-
-    if (!casID) {
-      if (sources.length === 1) {
-        casID = sources[0];
-      } else
-        throw new Error(
-          'There is more than one registered service provider, you must provide the casID for the service provider you wish to interact with'
-        );
-    }
-
-    const serviceProvider = this.services[casID];
-    if (!serviceProvider)
-      throw new Error(`No service provider was found for casID ${casID}`);
-
-    return serviceProvider;
+  public getSource(casID: string): CASSource | undefined {
+    return this.sourcesMap.get(casID);
   }
 
   /**
@@ -101,72 +55,15 @@ export class MultiSourceService {
    * @param source the source from which to get the object from
    * @returns the object if found, otherwise undefined
    */
-  public async getFromSource<O extends object>(
-    hash: string,
-    casID: string | undefined
-  ): Promise<Entity<O> | undefined> {
+  public async getFromSource(hashes: string[], casID: string): Promise<Entity<any>[]> {
     const source = this.getSource(casID);
-
-    const object: O | undefined = (await source.get(hash)) as O;
-
-    if (!object) return undefined;
-
-    if ((source as KnownSourcesSource).knownSources) {
-      // Get the links
-
-      setTimeout(async () => {
-        const result = await this.client.query({
-          query: gql`
-        {
-          entity(uref: "${hash}") {
-            id
-            _context {
-              patterns {
-                links {
-                  id
-                  __typename
-                }
-              }
-            }
-          }
-        }
-        `,
-        });
-
-        const linksResult = result.data.entity._context.patterns.links;
-        if (linksResult) {
-          // Discover the known sources from the links
-          const linksPromises = linksResult.map((link) =>
-            discoverKnownSources(this.localKnownSources)(
-              link.id,
-              link.__typename,
-              source as KnownSourcesSource
-            )
-          );
-          await Promise.all(linksPromises);
-        }
-      });
-    }
-
-    if (!object) {
-      // Object retrieval succeeded but object was not found,
-      // remove from the known sources
-      await this.localKnownSources.removeKnownSource(hash, source.casID);
-    }
-
-    return {
-      id: hash,
-      object,
-      casID,
-    };
+    if (!source) throw new Error(`Source ${casID} not found`);
+    return source.get(hashes);
   }
 
-  private async tryGetFromSources<O extends object>(
-    hash: string,
-    sources: string[]
-  ): Promise<Entity<O> | undefined> {
+  private async tryGetFromSources(hashes: string): Promise<Entity<O> | undefined> {
     const tryGetFromSource = async (sourceName: string) => {
-      const object = await this.getFromSource<O>(hash, sourceName);
+      const object = await this.getFromSource(hashes, sourceName);
       return object ? Promise.resolve(object) : Promise.reject();
     };
 
@@ -187,9 +84,7 @@ export class MultiSourceService {
    * @param hash the hash of the object to retrieve
    * @returns the object if found, otherwise undefined
    */
-  public async get<O extends object>(
-    hash: string
-  ): Promise<Entity<O> | undefined> {
+  public async get(hash: string): Promise<Entity<O> | undefined> {
     let knownSources: string[] | undefined = undefined;
 
     // If there is only one source, use that to get the object
@@ -212,9 +107,7 @@ export class MultiSourceService {
         const object = await this.tryGetFromSources<O>(hash, defaultSources);
 
         if (object) return object;
-        remainingCids = remainingCids.filter(
-          (casID) => !defaultSources.includes(casID)
-        );
+        remainingCids = remainingCids.filter((casID) => !defaultSources.includes(casID));
       }
 
       const finalObject = await this.tryGetFromSources<O>(hash, remainingCids);
@@ -235,10 +128,7 @@ export class MultiSourceService {
    * @param source
    * @param links
    */
-  public async postEntityUpdate(
-    source: KnownSourcesSource,
-    links: string[]
-  ): Promise<void> {
+  public async postEntityUpdate(source: KnownSourcesSource, links: string[]): Promise<void> {
     const knownSourcesService = source.knownSources;
     if (!knownSourcesService) return;
 
@@ -248,9 +138,7 @@ export class MultiSourceService {
 
       // If the only known source is the name of the source itself, we don't need to tell the provider
       const sameSource =
-        knownSources &&
-        knownSources.length === 1 &&
-        knownSources[0] === source.casID;
+        knownSources && knownSources.length === 1 && knownSources[0] === source.casID;
 
       if (knownSources && !sameSource) {
         await knownSourcesService.addKnownSources(link, knownSources);
@@ -266,9 +154,7 @@ export class MultiSourceService {
    * @param source
    * @param links
    */
-  public async postEntityCreate<O extends Object>(
-    entity: Entity<O>
-  ): Promise<void> {
+  public async postEntityCreate<O extends Object>(entity: Entity<O>): Promise<void> {
     if (!entity.casID) {
       throw new Error('casID for the entity is required and none was provided');
     }
