7457d9b7e2ebc177b91f52a015d9580c8fe9a962
Pepo <pepo@uprtcl.io>
2021-01-12 16:00:35 +0100

    remove unsused patterns

--- core/evees/src/patterns/behaviours/clone.ts
+++
@@ -1,5 +0,0 @@
-import { Behaviour } from '../types/behaviour';
-
-export interface Clone<T> extends Behaviour<T> {
-  clone: (pattern: T) => Promise<string>;
-}
--- core/evees/src/patterns/behaviours/has-text.ts
+++
@@ -1,5 +0,0 @@
-import { Behaviour } from '../types/behaviour';
-
-export interface HasText<T = any> extends Behaviour<T> {
-  text: (pattern: T) => string;
-}
